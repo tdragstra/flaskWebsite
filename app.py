17d802500dc86f57827d902d505635a4afa0ade4
TDRAGSTRA <TIMBO040@GMAIL.COM>
2022-11-11 14:32:25 +0100

    index pagina met form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import tim
 
 app = Flask(__name__)
@@ -11,6 +11,10 @@ def hello_world():
 def hello_world1():
     return "<p>2de pagina</p>"
 
-@app.route("/tim")
+@app.route("/tim", methods=['POST', 'GET'])
 def hello_world3():
-    return tim.helloTim()
+    if request.method == 'POST':
+        age = request.form['age']
+    name = "Tim"
+    lastName = "Dragstra"
+    return render_template('index.html', name=name, lastName = lastName)
